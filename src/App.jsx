e45082184c560f1e04d5b103b0b1c87a3343f787
ZohairTahir <zohairt24@gmail.com>
2024-02-24 19:40:22 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Home/homepage.jsx";
+import HomepageStudent from "./Pages/Home/homepageStudent.jsx";
 import Welcome from "./Pages/Welcome/welcomepage.jsx";
 import Signin from "./Pages/Signin/signin";
 import Profile from "./Pages/Profile/profile";
 import Register from "./Pages/Signin/register";
-
 import ProjectPost from "./Pages/ProjectPost/posting";
 import Profilesetup from "./Pages/Profile/ProfileSetup";
 function App() {
@@ -15,18 +15,11 @@ function App() {
       <>
         <Routes>
           <Route path="/homepage" element={<Homepage />} />
+          <Route path="/homepageStudent" element={<HomepageStudent />} /> 
           <Route path="/signin" element={<Signin />} />
           <Route path="/register" element={<Register />} />
           <Route path="/profilesetup" element={<Profilesetup />} />
-
-          <Route
-            path="/profile"
-            element={
-              <>
-                <Profile />
-              </>
-            }
-          />
+          <Route path="/profile" element={<><Profile /></>}/>
           <Route path="/post" element={<ProjectPost />} />
           <Route path="/" element={<Welcome />} />
         </Routes>
