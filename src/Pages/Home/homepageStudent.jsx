f606efad44cb4ab2672b6df8a18b897609a7f638
MuhammadGanny <111433961+MuhammadGanny@users.noreply.github.com>
2024-02-25 16:08:44 +0500

    CONNECT ERROR SOLVED

--- src/Pages/Home/homepageStudent.jsx
+++ src/Pages/Home/homepageStudent.jsx
@@ -1,10 +1,11 @@
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import LOGO from "../Assets/logo.svg";
+// import { Fragment } from "react";
+// import { Disclosure, Menu, Transition } from "@headlessui/react";
+// import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+// import LOGO from "../Assets/logo.svg";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Header from "../components/headerStudent";
+import Cookies from "js-cookie";
 import {
   Card,
   CardBody,
@@ -15,6 +16,7 @@ import {
 
 export default function Home() {
   const [projectPosts, setProjectPosts] = useState([]);
+  const [userIdFromCookie, setUserIdFromCookie] = useState("");
 
   useEffect(() => {
     axios
@@ -40,6 +42,21 @@ export default function Home() {
       // Show error message or handle error case
     }
   };
+  useEffect(() => {
+    // const setUserIdFromCookie = Cookies.get("userId");
+    // setUserIdFromCookie(userIdFromCookie);
+    const userIdFromCookie = Cookies.get("userId");
+    setUserIdFromCookie(userIdFromCookie);
+    const userTypeFromCookie = Cookies.get("userType");
+    const tokenFromCookie = Cookies.get("token");
+
+    console.log("User ID: in post area ", userIdFromCookie);
+    // console.log("User Type: in post area ", userTypeFromCookie);
+    // console.log("Token: in post area ", tokenFromCookie);
+    // } else {
+    //   console.log("ID type and Token Not avauilable in cookies ");
+    // }
+  }, []);
 
   return (
     <>
