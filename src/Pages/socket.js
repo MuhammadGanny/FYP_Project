1b86d7d8829c49f482fd477456903b9f25728077
MuhammadGanny <111433961+MuhammadGanny@users.noreply.github.com>
2024-10-02 12:15:29 +0500

    Updated socket.io connection URL

--- src/Pages/socket.js
+++ src/Pages/socket.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 // });
 //const socket = io("http://localhost:5000");
 
-const socket = io("https://fyp-project.vercel.app");
+const socket = io("https://fyp-project-eight.vercel.app");
 
 socket.on("connect", () => {
   console.log("Connected to Socket.IO server");
